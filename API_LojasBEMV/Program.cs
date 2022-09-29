cab24a35c194c581bc9bd8837cfc5d15abdb8e66
GiveMACode <91268632+GiveMACode@users.noreply.github.com>
2022-09-29 19:44:30 -0300

    Start the project

---
+++ API_LojasBEMV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API_LojasBEMV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
